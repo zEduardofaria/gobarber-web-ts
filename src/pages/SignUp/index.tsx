be6c3dfd69df788babd5f2b1aad3d205884b2485
Eduardo Faria <z.eduardofaria@gmail.com>
2020-05-19 15:20:56 -0300

    Singup page

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { FiArrowLeft, FiMail, FiUser, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Sign Up</h1>
+
+        <Input name="name" icon={FiUser} placeholder="Name" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+
+        <Button type="submit">Sign up</Button>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Back to logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
